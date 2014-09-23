ea69e2b859e484f82e9d200f2fdf67f31feae7fc
Scott Crespo <screspo@highcornergroup.com>
2014-09-22 21:54:31 -0400

    create build environment script

---
+++ buildenv.sh
@@ -0,0 +1,4 @@
+pip install django
+pip install git+https://github.com/django-nonrel/django@nonrel-1.5
+pip install git+https://github.com/django-nonrel/djangotoolbox
+pip install git+https://github.com/django-nonrel/mongodb-engine

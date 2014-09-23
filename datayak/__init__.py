9604f9d014938a932ba79d3fe300cc90e07671b8
Scott Crespo <screspo@highcornergroup.com>
2014-09-23 01:10:06 -0400

    configured app for mongo and ODM

--- datayak/__init__.py
+++ datayak/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
 app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB': "datayak"}
+app.config["SECRET_KEY"] = "8585143a612fb2b28f308bc334fa978c10cf6ab1"
+
+db = MongoEngine(app)
 
 if __name__ == '__main__':
 	app.run()

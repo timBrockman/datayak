f711f9d4124ce8613f7a4b54f076d40cd83500c0
Scott Crespo <screspo@highcornergroup.com>
2014-09-22 22:26:43 -0400

    modified builenv for django 1.7

--- sbin/buildenv.sh
+++ sbin/buildenv.sh
@@ -1,4 +1,1 @@
-pip install django
-pip install git+https://github.com/django-nonrel/django@nonrel-1.5
-pip install git+https://github.com/django-nonrel/djangotoolbox
-pip install git+https://github.com/django-nonrel/mongodb-engine
+pip install django==1.7
--- settings/base.py
+++ settings/base.py
@@ -1,33 +1,20 @@
 """
-Django settings for datayak project.
+Django base settings for datayak project.
+Use this settings file for local deployments only
 
-For more information on this file, see
-https://docs.djangoproject.com/en/1.6/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.6/ref/settings/
 """
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '*!e64e35-g(yoh4ofynn!=v^qe#dq93ira)c8$z3e!)xg*5pob'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
-# Application definition
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -51,19 +38,12 @@ ROOT_URLCONF = 'datayak.urls'
 
 WSGI_APPLICATION = 'datayak.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -74,9 +54,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'

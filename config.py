163b81c00d4e5b079ac05569c63b812292514d55
Scott Crespo <screspo@highcornergroup.com>
2014-09-24 01:12:36 -0400

    reformulated settings into confnfig file

--- config.py
+++ config.py
@@ -1,2 +1,21 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    SECRET_KEY = "8585143a612fb2b28f308bc334fa978c10cf6ab1"
+    
+
+class LocalConfig(Config):
+    DEBUG = True
+    DATABASE = {
+              NAME:'datayak',
+              USERNAME:'theyak',
+              PASSWORD:'',
+              HOST:'locahost',
+              PORT:':27017',
+              }
+   
+config = {
+          'local': LocalConfig,
+          'default': LocalConfig,
+        } 
--- datayak/__init__.py
+++ datayak/__init__.py
@@ -1,7 +1,4 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "8585143a612fb2b28f308bc334fa978c10cf6ab1"
 
-if __name__ == '__main__':
-	app.run()

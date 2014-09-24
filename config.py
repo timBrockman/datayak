0367337886eec2c2e1893c99bdb3a87501c2629c
Scott Crespo <screspo@highcornergroup.com>
2014-09-24 01:42:17 -0400

    working out app initialization

--- config.py
+++ config.py
@@ -4,7 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = "8585143a612fb2b28f308bc334fa978c10cf6ab1"
     
-
 class LocalConfig(Config):
     DEBUG = True
     DATABASE = {
--- datayak/__init__.py
+++ datayak/__init__.py
@@ -1,4 +1,15 @@
 from flask import Flask
+from config import *
+from pymongo import MongoClient
 
-app = Flask(__name__)
+def connect_db():
+    c = MongoClient(DATABASE['HOST'],DATABASE['PORT'])
+    c = c.DATABASE['NAME']
+    
+    return c
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    
 

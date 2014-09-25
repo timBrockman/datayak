229f3d105d8c5ba5611201d3986a4eace8fb6261
Scott Crespo <screspo@highcornergroup.com>
2014-09-24 23:39:10 -0400

    WIP on mange.py runserver

--- datayak/manage.py
+++ datayak/manage.py
@@ -1,1 +1,10 @@
-# manage.py for creating custom commands
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from flask.ext.script import Manager, Server
+from datayak import create_app
+
+manager = Manager(create_app('local')) 
+manager.add_command('runserver', Server())
+
+if __name__ == "__main__":
+    manager.run()

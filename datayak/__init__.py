92065902b7af8794602318540e92b0475922ab11
Scott Crespo <screspo@highcornergroup.com>
2014-09-24 21:40:11 -0400

    funct must return app

--- datayak/__init__.py
+++ datayak/__init__.py
@@ -2,14 +2,12 @@ from flask import Flask
 from config import *
 from flask.ext.pymongo import PyMongo
 
-def connect_db():
-    c = MongoClient(DATABASE['HOST'],DATABASE['PORT'])
-    c = c.DATABASE['NAME']
-    
-    return c
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+    # Flask-PyMongo Reads from Config to establish connections
     mongo=PyMongo(app)
+    
+    return app
 

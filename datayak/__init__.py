4f0af55073d05a6017dba323575e28f879787fdd
Scott Crespo <screspo@highcornergroup.com>
2014-09-26 00:04:14 -0400

    modified comment

--- datayak/__init__.py
+++ datayak/__init__.py
@@ -4,7 +4,7 @@ from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 app.config.from_object(config['local'])
-# Flask-PyMongo Reads from Config to establish connections
+# Flask-PyMongo Automatically Reads from Config to establish connections
 mongo = PyMongo(app)
 from .views import yak
 app.register_blueprint(yak)

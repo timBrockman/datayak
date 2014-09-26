b81fbace599c52b11f60a40f40534aeca7e68f20
Scott Crespo <screspo@highcornergroup.com>
2014-09-26 15:41:38 -0400

    created utils file

--- datayak/models.py
+++ datayak/models.py
@@ -1,3 +1,12 @@
 from flask.ext.pymongo import PyMongo
+from datayak import mongo
 
-mongo = PyMongo()
+
+
+class Yak(object):
+    """
+    The Yak Model represents a collection in mongo, which contains the Yak's data 
+    including the groups he belongs to and his data.
+    """
+    
+    # create dictionary keys for a Yak document

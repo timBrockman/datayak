22faf4b6aa4ec4bdfe85a621f53a3190bf211c7c
Scott Crespo <screspo@highcornergroup.com>
2014-09-28 21:43:18 -0400

    developing group scraper

--- datayak/models.py
+++ datayak/models.py
@@ -1,5 +1,6 @@
 from flask.ext.pymongo import PyMongo
-from datayak import mongo
+from datayak import db
+import requests, json
 
 class Yak(object):
     """
@@ -8,4 +9,28 @@ class Yak(object):
     """
     
     # create dictionary keys for a Yak document
-    def create(self, **dict ):
+    def find_groups():
+        """
+        Perform raw_text search for groups containing technology in title, category, or description
+        """
+        
+        url = 'https://api.meetup.com/find/groups'
+        params = {
+                  'sign':'true', 
+                  'key':app.config['API_KEY'], 
+                  'member_id':app.config['MEMBER_ID'], 
+                  'text':'technology',
+                  'zip':'32801', 
+                  'radius':'30', 
+                  'page':'999',
+                  }
+
+        
+        # Make the API Request, transform response to native Python Types
+        response = requests.get(url,params=params).json()
+        
+        # Save each group contained in the response as a Mongo document
+        for item in response:
+            db.groups.insert(item)
+            
+        print 'completed'
--- datayak/utils.py
+++ datayak/utils.py
@@ -2,32 +2,3 @@
 from flask.ext.pymongo import PyMongo
 from datayak import app, mongo
 import bson, requests, json
-
-def upsert_yak():
-    """
-    Creates the Yak document if it doesn't exist. 
-    Or, Updates the existing Yak document to remain current with
-    his groups
-    """
-    
-    url = 'https://api.meetup.com/2/groups.json'
-    
-    params = {
-              'key':app.config['API_KEY'],
-              'sign':'true',
-              'member_id':app.config['MEMBER_ID'],
-              }
-    
-    # Make the API Request
-    data = requests.get(url,params=params)
-    
-    #convert JSON response to python dictionary
-    data = data.json()
-        
-    datayak = {
-               'name':name,
-               'email':email,
-               'password':password,
-               'api_key':api_key,
-               'groups':''
-               }
